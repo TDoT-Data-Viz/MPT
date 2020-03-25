bfa189e8e0da00d16788f81c948b03c1302cce02
Jared Cleghorn <jaredcleghorn@gmail.com>
2020-03-25 11:56:51 -0500

    Pep 8 formatting for demand

--- Scripts/demand.py
+++ Scripts/demand.py
@@ -5,7 +5,7 @@ import time
 from arcpy import env
 
 
-def Demand(planning_area, inputs, output):
+def demand(planning_area, inputs, output):
     """Function that automates the Demand component of the MPT.
 
     Required parameters:
@@ -16,7 +16,7 @@ def Demand(planning_area, inputs, output):
     bike distance groups = "0 5280 5;5280 10560 4;10560 15840 3;15840 21120 2;21120 1000000 1"
     ped distance groups = "0 1320 5;1320 2640 4;2640 3960 3;3960 5280 2;5280 1000000 1"
 
-    TODO: Eloborate on the full script function.
+    TODO: Elaborate on the full script function.
     """
     # Saves the start time so script duration can be calculated.
     start_time = time.time()
@@ -44,27 +44,27 @@ def Demand(planning_area, inputs, output):
 
     # Run Euclidean Distance for each point feature in 'inputs' database and Reclassify 1-5.
     for fc in ed_features:
-        edLayer = arcpy.gp.EucDistance_sa(os.path.join(inputs, fc), os.path.join(output, "rED_"+fc), "")
+        ed_layer = arcpy.gp.EucDistance_sa(os.path.join(inputs, fc), os.path.join(output, "rED_"+fc), "")
         # The values can be changed to create Bike or Ped Demand layer.
         # TODO: Make distance groups a parameter of the function.
-        arcpy.gp.Reclassify_sa(edLayer, "VALUE", "0 1320 5;1320 2640 4;2640 3960 3;3960 5280 2;5280 1000000 1",
+        arcpy.gp.Reclassify_sa(ed_layer, "VALUE", "0 1320 5;1320 2640 4;2640 3960 3;3960 5280 2;5280 1000000 1",
                                os.path.join(output, "rWS_"+fc), "DATA")
         # This clears the memory so you don't run out.
-        del edLayer
+        del ed_layer
 
     # Create rasters for "Pop_Density", "Employ_Density", and "Active_Commute" found in BG_2017
     # Note: In BG_2017 Employ_Density field name is actually "Employ_D"
     block_group = os.path.join(inputs, 'BG_2017')
     print("Starting polygon to raster conversions")
-    rPopD = arcpy.PolygonToRaster_conversion(block_group, "Pop_Density", os.path.join(output, 'rPopD'), "CELL_CENTER", "NONE")
-    rCommute = arcpy.PolygonToRaster_conversion(block_group, "Active_Commute", os.path.join(output, 'rCommute'), "CELL_CENTER", "NONE")
-    rEmpD = arcpy.PolygonToRaster_conversion(block_group, "Employ_D", os.path.join(output, 'rEmpD'), "CELL_CENTER", "NONE")
+    r_pop_den = arcpy.PolygonToRaster_conversion(block_group, "Pop_Density", os.path.join(output, 'r_pop_den'), "CELL_CENTER", "NONE")
+    r_commute = arcpy.PolygonToRaster_conversion(block_group, "Active_Commute", os.path.join(output, 'r_commute'), "CELL_CENTER", "NONE")
+    r_employ_den = arcpy.PolygonToRaster_conversion(block_group, "Employ_D", os.path.join(output, 'r_employ_den'), "CELL_CENTER", "NONE")
 
     # Reclassify Pop, Emp, Commute using Jenks
     print("Starting Slices")
-    arcpy.gp.Slice_sa(rPopD, os.path.join(output, 'rWS_PopDensity'), "5" ,"NATURAL_BREAKS", "1" )
-    arcpy.gp.Slice_sa(rEmpD, os.path.join(output, 'rWS_EmpDensity'), "5" ,"NATURAL_BREAKS", "1" )
-    arcpy.gp.Slice_sa(rCommute, os.path.join(output, 'rWS_Commute'), "5" ,"NATURAL_BREAKS", "1" )
+    arcpy.gp.Slice_sa(r_pop_den, os.path.join(output, 'rWS_PopDensity'), "5" ,"NATURAL_BREAKS", "1" )
+    arcpy.gp.Slice_sa(r_employ_den, os.path.join(output, 'rWS_EmpDensity'), "5" ,"NATURAL_BREAKS", "1" )
+    arcpy.gp.Slice_sa(r_commute, os.path.join(output, 'rWS_Commute'), "5" ,"NATURAL_BREAKS", "1" )
     print("Starting LC Reclass")
     # Reclassify Land Cover
     arcpy.gp.Reclassify_sa(os.path.join(inputs, 'ActiveLC'), "NLCD_Land_Cover_Class",
@@ -74,30 +74,30 @@ def Demand(planning_area, inputs, output):
 
     # Build weighted sum table
     env.workspace = output
-    WSTable = [[raster, "VALUE", 1] for raster in arcpy.ListRasters("rWS_*")]
+    ws_table = [[raster, "VALUE", 1] for raster in arcpy.ListRasters("rWS_*")]
 
     # Create weighted sum
-    WSumTableObj = arcpy.sa.WSTable(WSTable)
-    outWS = arcpy.sa.WeightedSum(WSumTableObj)
-    outWS.save(os.path.join(output, "Weighted_Sum"))
+    ws_table_obj = arcpy.sa.ws_table(ws_table)
+    out_ws = arcpy.sa.WeightedSum(ws_table_obj)
+    out_ws.save(os.path.join(output, "Weighted_Sum"))
 
     # Reclassify Weighted Sum 
-    WS_Slice = arcpy.gp.Slice_sa(outWS, os.path.join(output, "WS_Slice"), "5", "NATURAL_BREAKS", "1")
+    ws_slice = arcpy.gp.Slice_sa(out_ws, os.path.join(output, "ws_slice"), "5", "NATURAL_BREAKS", "1")
 
     # Zonal Statistics using Blocks
-    ZS = arcpy.gp.ZonalStatistics_sa(os.path.join(inputs, "Blocks"), "OBJECTID", WS_Slice,
+    zonal_stats = arcpy.gp.ZonalStatistics_sa(os.path.join(inputs, "Blocks"), "OBJECTID", ws_slice,
                                      os.path.join(output, "ZonalSt_Demand"), "MEAN", "DATA")
     
     # Reclassify Zonal Statistics
-    FD = arcpy.gp.Slice_sa(ZS, os.path.join(output, "FinalDemand"), "5", "NATURAL_BREAKS", "1")
+    final_demand = arcpy.gp.Slice_sa(zonal_stats, os.path.join(output, "FinalDemand"), "5", "NATURAL_BREAKS", "1")
     
     # Convert FinalDemand to Polygon
-    arcpy.RasterToPolygon_conversion(FD, os.path.join(output, "Demand_Polygons"), "SIMPLIFY", "Value", "")
+    arcpy.RasterToPolygon_conversion(final_demand, os.path.join(output, "Demand_Polygons"), "SIMPLIFY", "Value", "")
     
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
 # Example
-Demand(r"C:\Users\jj10048\Desktop\Projects\MSI\SupplyScriptTest\Demand.gdb\SW",
+demand(r"C:\Users\jj10048\Desktop\Projects\MSI\SupplyScriptTest\Demand.gdb\SW",
        r"C:\Users\jj10048\Desktop\Projects\MSI\SupplyScriptTest\Demand.gdb",
        r"C:\Users\jj10048\Desktop\Projects\MSI\SupplyScriptTest\Ped_Statewide.gdb")
